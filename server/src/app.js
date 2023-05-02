ba378bf90f7c2fdc447a1d46f69bff1196c1540a
kmHan <kmhan@smartcoop.kr>
2023-05-02 22:34:14 +0900

    client add

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
+const messages = [{
+    id: 0,
+    text: "abc"
+}]
+
+
 // CORS설정
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(cors());
 
 // morgan 로그관리
 app.use(morgan('combined'))
@@ -18,6 +22,18 @@ app.use(express.json());
 //정적 파일 제공하기 빌드해서 퍼블릭에 넣어야함
 //app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.get('/textarea', (req, res)=>{
+    res.send(messages);
+});
+
+app.post('/textarea', (req, res)=>{
+    const newMessage = req.body;
+    messages.push({
+        id: messages.length,
+        text: newMessage,
+    })
+    res.send(newMessage);
+})
 
 app.get('/', (req, res) => {
     res.send("hello server")
