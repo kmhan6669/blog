00fcca0d70d744e83475cc3f01a631c0ee80a613
kmHan <kmhan@smartcoop.kr>
2023-05-03 00:05:40 +0900

    더미데이터 작성 및 /textarea get,post 요청

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,42 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const contents = [{
+  id: 0,
+  date : new Date(),
+  creator: '',
+  ops: [
+    {
+      attributes: {
+        bold: true
+      },
+      insert: "이거 되는거 맞아?"
+    },
+    {
+    attributes: {
+        header: 1
+      },
+      insert: "\n"
+    },
+    {
+      attributes: {
+        italic: true
+      },
+      insert: "헐 이게 되네ㄷㄷ"
+    },
+    {
+      attributes: {
+        header: 2
+      },
+      insert: "\n"
+    },
+    {
+      insert: "대박\n"
+    }
+  ]
+}];
+
 const app = express();
-
-const messages = [{
-    id: 0,
-    text: "abc"
-}]
-
-
 // CORS설정
 app.use(cors());
 
@@ -23,22 +51,26 @@ app.use(express.json());
 //app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/textarea', (req, res)=>{
-    res.send(messages);
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.send(contents);
 });
 
-app.post('/textarea', (req, res)=>{
-    const newMessage = req.body;
-    messages.push({
-        id: messages.length,
-        text: newMessage,
-    })
-    res.send(newMessage);
+app.prependOnceListener('/textarea', (req, res)=>{
+  const newContents = req.body;
+  contents.push({
+    id: contents.length,
+    ...newContents,
+  });
+  res.setHeader(201,'Content-Type', 'application/json; charset=utf-8')
+  res.send(contents[contents.length-1]);
 })
 
+
+
 app.get('/', (req, res) => {
-    res.send("hello server")
+  res.send("hello server")
 });
 
 app.listen(8000, ()=>{
-    console.log('listening on port 8000...')
+  console.log('listening on port 8000...')
 });
