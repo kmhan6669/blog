bbb2dbd6e9a466a1f9dffa44446cf0153af0ea9b
Seungmin Lee <seungmin.lee@business-canvas.com>
2023-05-04 11:41:17 +0900

    router test

---
+++ client/src/PostList.js
@@ -0,0 +1,14 @@
+import PostListItem from "./PostListItem";
+
+function PostList(){
+    return(
+        <>
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+        </>
+    )
+}
+
+export default PostList;
