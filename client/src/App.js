ac0212500e7a7f78aae16245c6edccdf7b5bf68b
Seungmin Lee <seungmin.lee@business-canvas.com>
2023-05-06 00:43:40 +0900

    스타일링 조금 시도

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import PostList from "./PostList";
 import { useState,useEffect } from "react";
 import axios from "axios";
+import './style.css'
+import styled from "styled-components";
 
 
 function App() {
@@ -18,13 +20,19 @@ function App() {
     
   }, [])
 
+  const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `
 
   return (
     <div className="App">
-      <div>
+      <Header>
         <h1>Blog</h1>
-        <Link to={'newpost'} >새 포스트 작성</Link>
-      </div>
+        <Link className="btn" to={'newpost'} >새 포스트 작성</Link>
+      </Header>
       <PostList posts={posts}/>
     </div>
   );
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,14 +1,25 @@
 import PostListItem from "./PostListItem";
+import styled, {css} from "styled-components";
+
+const PostListWrapper = styled.div`
+    --auto-grid-min-size: 20rem;
+    display: grid;
+    grid-gap: 1.5rem;
+    grid-template-columns: repeat(auto-fill, minmax(var(--auto-grid-min-size), 1fr));
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+`
 
 function PostList({ posts }){
     return(
-        <>
+        <PostListWrapper>
         {
             posts?.map((post)=>(
                 <PostListItem post={post}/>
             ))
         }
-        </>
+        </PostListWrapper>
     )
 }
 
